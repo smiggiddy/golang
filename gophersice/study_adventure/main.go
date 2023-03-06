ed570754cc7523109e207780a4059327deda7b82
Mike <smiggiddy@gmail.com>
2023-03-06 08:52:52 -0500

    added readme

--- gophersice/study_adventure/main.go
+++ gophersice/study_adventure/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/smiggiddy/golang/gophercise/study_adventure/studyguide"
+import (
+	"fmt"
+
+	"github.com/smiggiddy/golang/gophercise/study_adventure/studyguide"
+)
 
 func main() {
 	studyguide.Parse()
+	fmt.Println("This is from main")
 }
