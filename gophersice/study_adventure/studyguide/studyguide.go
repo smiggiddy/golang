56e77fc49a16b070fe4a64f59eb2cda33a399b08
Mike Smith <smig@Mikes-Mac-mini.lab.smig.tech>
2023-03-21 13:16:44 -0400

    updating code

--- gophersice/study_adventure/studyguide/studyguide.go
+++ gophersice/study_adventure/studyguide/studyguide.go
@@ -1,7 +1,25 @@
 package studyguide
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 func Parse() {
 	fmt.Println("This is just some starter code")
+	var b []byte
+
+	b, err := os.ReadFile("/home/smig/repos/gophercises/study_adventure/yamlvalues.yaml")
+	if err != nil {
+		fmt.Println("Something went wrong with that file boss")
+	}
+
+	var f interface{}
+
+	jsonERR := json.Unmarshal(b, &f)
+	if jsonERR != nil {
+		fmt.Println("Something went wrong with that file boss")
+	}
+	fmt.Println(f)
 }
