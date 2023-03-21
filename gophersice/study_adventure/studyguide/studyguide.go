c27a500aad7d2aded97c05726cb3bce3754ea3dc
Mike Smith <smig@Mikes-Mac-mini.lab.smig.tech>
2023-03-21 14:08:10 -0400

    added studyData strut

--- gophersice/study_adventure/studyguide/studyguide.go
+++ gophersice/study_adventure/studyguide/studyguide.go
@@ -7,19 +7,67 @@ import (
 )
 
 func Parse() {
-	fmt.Println("This is just some starter code")
 	var b []byte
 
-	b, err := os.ReadFile("/home/smig/repos/gophercises/study_adventure/yamlvalues.yaml")
+	b, err := os.ReadFile("/Users/smig/repos/github-golang/gophersice/study_adventure/studyguide.json")
 	if err != nil {
 		fmt.Println("Something went wrong with that file boss")
 	}
 
-	var f interface{}
-
+	// var f interface{}
+	var f studyData
 	jsonERR := json.Unmarshal(b, &f)
 	if jsonERR != nil {
 		fmt.Println("Something went wrong with that file boss")
 	}
-	fmt.Println(f)
+	fmt.Println(f.Intro.Title)
+
+}
+
+type studyData struct {
+	Intro struct {
+		Title   string   `json:"title"`
+		Desc    string   `json:"desc"`
+		About   []string `json:"tips"`
+		Options []struct {
+			Text string `json:"text"`
+			Sect string `json:"sect"`
+		} `json:"options"`
+	} `json:"start"`
+	CKS struct {
+		Title   string   `json:"title"`
+		Desc    string   `json:"desc"`
+		About   []string `json:"tips"`
+		Options []struct {
+			Text string `json:"text"`
+			Sect string `json:"sect"`
+		} `json:"options"`
+	} `json:"cks"`
+	AWS struct {
+		Title   string   `json:"title"`
+		Desc    string   `json:"desc"`
+		About   []string `json:"tips"`
+		Options []struct {
+			Text string `json:"text"`
+			Sect string `json:"sect"`
+		} `json:"options"`
+	} `json:"aws"`
+	CKSVim struct {
+		Title   string   `json:"title"`
+		Desc    string   `json:"desc"`
+		About   []string `json:"tips"`
+		Options []struct {
+			Text string `json:"text"`
+			Sect string `json:"sect"`
+		} `json:"options"`
+	} `json:"cks-vim"`
+	CKSLinks struct {
+		Title   string   `json:"title"`
+		Desc    string   `json:"desc"`
+		About   []string `json:"tips"`
+		Options []struct {
+			Text string `json:"text"`
+			Sect string `json:"sect"`
+		} `json:"options"`
+	} `json:"cks-links"`
 }
