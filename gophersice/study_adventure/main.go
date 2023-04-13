358c28d222fc5196c9943600d174d3072f20d068
Mike Smith <smig@Mikes-Mac-mini.lab.smig.tech>
2023-04-13 15:19:32 -0400

    completed program

--- gophersice/study_adventure/main.go
+++ gophersice/study_adventure/main.go
@@ -1,36 +1,54 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/smiggiddy/golang/gophercise/study_adventure/studyguide"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// sg := studyguide.Parse("./studyguide.json")
-	// t, _ := template.ParseFiles("templates/main.html")
-	// t.Execute(w, sg)
+func renderTemplate(w http.ResponseWriter, tmpl string, s studyguide.Study) {
+	t, _ := template.ParseFiles("templates/" + tmpl + ".html")
+	t.Execute(w, s)
+}
 
-	// studyguide.RenderTemplate(w, r, "main", sg)
-	fmt.Fprintf(w, "Welcome to the stuff")
+func homeHandler(w http.ResponseWriter, r *http.Request, t studyguide.Topics) {
+	renderTemplate(w, "main", t["start"])
+}
+
+func cksHandler(w http.ResponseWriter, r *http.Request, t studyguide.Topics) {
+	renderTemplate(w, "main", t["cks"])
+}
+
+func cksvimHandler(w http.ResponseWriter, r *http.Request, t studyguide.Topics) {
+	renderTemplate(w, "main", t["cks-vim"])
+}
+
+func ckslinksHandler(w http.ResponseWriter, r *http.Request, t studyguide.Topics) {
+	renderTemplate(w, "main", t["cks-links"])
+}
+
+func awsHandler(w http.ResponseWriter, r *http.Request, t studyguide.Topics) {
+	renderTemplate(w, "main", t["aws"])
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, studyguide.Topics)) http.HandlerFunc {
+	sg := studyguide.Parse("./studyguide.json")
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Here we will extract the page title from the Request,
+		// and call the provided handler 'fn'
+		fn(w, r, sg)
+	}
 
 }
 
-// func storyHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf("testing")
-// }
-
 func main() {
-	// studyData := studyguide.Parse()
-
-	// var studyTopics studyguide.Topics
-
-	studyTopics := studyguide.Parse("./studyguide.json")
-
-	fmt.Println(studyTopics)
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", makeHandler(homeHandler))
+	http.HandleFunc("/cks", makeHandler(cksHandler))
+	http.HandleFunc("/cks-vim", makeHandler(cksvimHandler))
+	http.HandleFunc("/cks-links", makeHandler(ckslinksHandler))
+	http.HandleFunc("/aws", makeHandler(awsHandler))
 	// http.HandleFunc("/story", storyHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
--- gophersice/study_adventure/studyguide/studyguide.go
+++ gophersice/study_adventure/studyguide/studyguide.go
@@ -3,26 +3,11 @@ package studyguide
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
-	"text/template"
 )
 
 func Parse(file string) Topics {
 	var Data Topics
-
-	// b, err := os.ReadFile(file)
-	// if err != nil {
-	// 	fmt.Println("Something went wrong with that file boss")
-	// }
-
-	// // var f interface{}
-	// var F studyData
-	// jsonERR := json.Unmarshal(b, &F)
-	// if jsonERR != nil {
-	// 	fmt.Println("Something went wrong with that file boss")
-	// }
-	// return F
 
 	f, err := os.Open(file)
 	if err != nil {
@@ -34,11 +19,6 @@ func Parse(file string) Topics {
 		fmt.Println(err)
 	}
 	return Data
-}
-
-func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, s studyData) {
-	t, _ := template.ParseFiles("templates/" + tmpl + ".html")
-	t.Execute(w, s)
 }
 
 type Topics map[string]Study
@@ -53,52 +33,4 @@ type Study struct {
 type StudyOptions struct {
 	Text string `json:"text"`
 	Sect string `json:"sect"`
-}
-
-type studyData struct {
-	Intro struct {
-		Title   string   `json:"title"`
-		Desc    string   `json:"desc"`
-		About   []string `json:"tips"`
-		Options []struct {
-			Text string `json:"text"`
-			Sect string `json:"sect"`
-		} `json:"options"`
-	} `json:"start"`
-	CKS struct {
-		Title   string   `json:"title"`
-		Desc    string   `json:"desc"`
-		About   []string `json:"tips"`
-		Options []struct {
-			Text string `json:"text"`
-			Sect string `json:"sect"`
-		} `json:"options"`
-	} `json:"cks"`
-	AWS struct {
-		Title   string   `json:"title"`
-		Desc    string   `json:"desc"`
-		About   []string `json:"tips"`
-		Options []struct {
-			Text string `json:"text"`
-			Sect string `json:"sect"`
-		} `json:"options"`
-	} `json:"aws"`
-	CKSVim struct {
-		Title   string   `json:"title"`
-		Desc    string   `json:"desc"`
-		About   []string `json:"tips"`
-		Options []struct {
-			Text string `json:"text"`
-			Sect string `json:"sect"`
-		} `json:"options"`
-	} `json:"cks-vim"`
-	CKSLinks struct {
-		Title   string   `json:"title"`
-		Desc    string   `json:"desc"`
-		About   []string `json:"tips"`
-		Options []struct {
-			Text string `json:"text"`
-			Sect string `json:"sect"`
-		} `json:"options"`
-	} `json:"cks-links"`
 }
